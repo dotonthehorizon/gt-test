afdc5ff8fdacdb79c05453b77ab2005eafb7a2f8
Ed McConnell <ed.mcconnell@gmail.com>
2019-06-14 12:04:08 +0100

    supported python versions

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-"""A setuptools based setup module for GTrends-Test"""
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""A setuptools based setup module for GTrends-Test"""
 
 from codecs import open
 from os import path
@@ -48,12 +49,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements,
